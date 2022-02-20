19eef16309aa8750e72263ee6001174c1eaa5359
thefwboy <xk18830084945@gmail.com>
2022-02-20 13:47:40 -0500

    volcontrol

--- config.def.h
+++ config.def.h
@@ -60,9 +60,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *voldowncmd[]  = { "voldown.sh", NULL };
+static const char *volupcmd[]  = { "volup.sh", NULL };
+static const char *voltogglecmd[]  = { "/home/thefwboy/Documents/Scripts/voltoggle.sh", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_F6,     spawn,          {.v = voltogglecmd } },
+	{ MODKEY,                       XK_F7,     spawn,          {.v = voldowncmd } },
+	{ MODKEY,                       XK_F8,     spawn,          {.v = volupcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
